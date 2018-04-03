afde02804f407ae6b8b94bbf662f10bcf79ad08e
iwen <1044803551@qq.com>
2018-04-03 14:51:32 +0800

    11
    11

--- js/font.js
+++ js/font.js
@@ -1,18 +1,33 @@
-(function (doc, win) {
-        var docEl = doc.documentElement,
-            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-        recalc = function () {
-            var clientWidth = docEl.clientWidth;
-            if (!clientWidth) return;
-            if(clientWidth>=640){
-                docEl.style.fontSize = '100px';
-            }else{
-                docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
-            }
-        };
+function mob(){
+	var iScale = 1;
+	iScale = iScale / window.devicePixelRatio;
+	document.write('<meta name="viewport" content="width=device-width,initial-scale='+iScale+',minimum-scale='+iScale+',maximum-scale='+iScale+',user-scalable=no" />')
+	var iWidth = document.documentElement.clientWidth;
+	iWidth = iWidth > 1620 ? 1620 : iWidth;
+	document.getElementsByTagName('html')[0].style.fontSize = iWidth / 16 + 'px';	
+}
+function pc(){
+	document.write('<meta name="viewport" content="width=device-width,initial-scale='+1+',minimum-scale='+1+',maximum-scale='+1+',user-scalable=no" />')
+	var iWidth = document.documentElement.clientWidth;
+	iWidth = iWidth > 640 ? 640 : iWidth;
+	document.getElementsByTagName('html')[0].style.fontSize = iWidth / 16 + 'px';
+	console.log(0)	
+}
+function browserRedirect() {
+	var sUserAgent = navigator.userAgent.toLowerCase();
+	var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+	var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+	var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+	var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+	var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+	var bIsAndroid = sUserAgent.match(/android/i) == "android";
+	var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+	var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+	if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+		mob();
+	} else {
+		pc();
+	}
+}
 
-    if (!doc.addEventListener) return;
-    win.addEventListener(resizeEvt, recalc, false);
-    doc.addEventListener('DOMContentLoaded', recalc, false);
-})(document, window);
-
+browserRedirect();
